c7e16dddac56966ecd57d84655f1d8cdee8e2f08
芊芃 <chienen@qianpengdeMacBook-Air.local>
2023-12-11 08:57:45 +0800

    fix bug in background

--- app/(public)/(routes)/preview/[documentId]/page.tsx
+++ app/(public)/(routes)/preview/[documentId]/page.tsx
@@ -52,7 +52,7 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
   }
 
   return (
-    <div className="pb-40 bg-[#1F1F1F]">
+    <div className="pb-40 ">
       <Cover preview url={document.coverImage} />
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
         <Toolbar preview initialData={document} />
